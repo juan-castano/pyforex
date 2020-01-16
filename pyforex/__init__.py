1666965ce93d2f35ecc26426709ec0303cc124af
Juan Castaño <juancastano.ing@gmail.com>
2020-01-16 02:44:24 -0500

    Adde new CONST path App

--- pyforex/__init__.py
+++ pyforex/__init__.py
@@ -3,6 +3,7 @@ from pyforex.domain.telegram import TelegramDomain
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+LIB_DIR = os.path.join(ROOT_DIR, "lib")
 
 print("::: PYFOREX :::")
 print(">>> Working on {}".format(ROOT_DIR))
